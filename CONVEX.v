52ff6e52ef1476e22e31c428855eae489819a5b7
Sheng-Yu-Cheng <sean.sheng.yu.cheng@gmail.com>
2026-03-21 13:08:13 +0800

    convex.v update

---
+++ CONVEX.v
@@ -0,0 +1,9 @@
+module CONVEX (CLK, RST, PT_XY, READ_PT, DROP_X, DROP_Y, DROP_V);
+    input CLK;
+    input RST;
+    input [4:0] PT_XY;
+    output reg READ_PT;
+    output reg [9:0] DROP_X;
+    output reg [9:0] DROP_Y;
+    output reg DROP_V;
+endmodule
